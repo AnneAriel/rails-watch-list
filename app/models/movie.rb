c29824514354dd0ac5ad32797d7f27bad823596c
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-11-17 11:46:55 +0100

    add list model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+  validates :rating, presence: true
+  validates :poster_url, presence: true
 end
