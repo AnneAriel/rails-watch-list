acf2c8f6f7d253d75e1b0287e89fc59dd4ae80c3
AnneAriel Ceyrac <anneariel.ceyrac@gmail.com>
2022-11-18 13:12:30 +0100

    ok

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
   validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true, uniqueness: true
-  validates :rating, presence: true
-  validates :poster_url, presence: true
+  validates :overview, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :movies, except:[:edit, :update, :destroy]
-  resources :lists, except:[:edit, :update, :destroy]
+  root "lists#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
